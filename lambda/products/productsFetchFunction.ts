3a2f7d6280c1ad2f34a0b044de861c2e18599d54
Rodrigo Elias <rodrigo.elias@associate.us.idemia.com>
2023-11-27 16:45:37 -0300

    Integrates Lambda fetch w/ DB

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -1,4 +1,14 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import { ProductRepository } from "/opt/nodejs/productsLayer";
+import { DynamoDB } from "aws-sdk";
+
+// Recupera nome da tabela através do env
+const productsDdb = process.env.PRODUCTS_DDB!;
+// Inicia client do DB
+const ddbClient = new DynamoDB.DocumentClient();
+
+// Inicia Product Repository
+const productRepository = new ProductRepository(ddbClient, productsDdb);
 
 // Lambda function responsável pelas busca de produtos
 export async function handler(
@@ -14,24 +24,33 @@ export async function handler(
     if (event.resource === "/products") {
         if(method === "GET") {
             console.log("Método GET chamado");
+
+            const products = await productRepository.getAllProducts();
             
             return {
                 statusCode: 200,
-                body: JSON.stringify({
-                    message: "GET Products - OK"
-                })
+                body: JSON.stringify(products)
             }
         }
     } else if (event.resource === "/products/{id}") {
-        const productId = event.pathParameters!.id;
+        const productId = event.pathParameters!.id as string;
 
         console.log(`Método GET /products/${productId}`);
+
+        try {
+            const product = await productRepository.getProductById(productId);
             
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: `Método GET /products/${productId} - OK`
-            })
+            return {
+                statusCode: 200,
+                body: JSON.stringify(product)
+            }
+        } catch (error) {
+            console.error((<Error>error).message);
+            
+            return {
+                statusCode: 404,
+                body: (<Error>error).message
+            }
         }
     }
 
