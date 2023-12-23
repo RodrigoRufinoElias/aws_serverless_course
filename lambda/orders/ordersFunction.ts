42943c52d0bfce8de3fe458f4c00da362d198f59
Rodrigo Elias <rodrigo.elias@associate.us.idemia.com>
2023-12-23 12:40:14 -0300

    Add orders services

--- lambda/orders/ordersFunction.ts
+++ lambda/orders/ordersFunction.ts
@@ -1,9 +1,20 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from "aws-lambda";
 import { Order, OrderRepository } from "/opt/nodejs/ordersLayer";
 import { Product, ProductRepository } from "/opt/nodejs/productsLayer";
 import { DynamoDB } from "aws-sdk";
 import * as AWSXRay from "aws-xray-sdk";
-import { OrderProductResponse, OrderRequest } from "./layers/orderApiLayer/nodejs/orderApi";
+import {
+  CarrierType,
+  OrderProductResponse,
+  OrderRequest,
+  OrderResponse,
+  PaymentType,
+  ShippingType,
+} from "./layers/orderApiLayer/nodejs/orderApi";
 
 // Usa o XRay para capturar o tempo de execução de tudo oq consome o "aws-sdk"
 AWSXRay.captureAWS(require("aws-sdk"));
@@ -23,70 +34,162 @@ const productRepository = new ProductRepository(ddbClient, productsDdb);
 
 // Lambda function responsável pela gestão de pedidos
 export async function handler(
-    event: APIGatewayProxyEvent, 
-    context: Context) : Promise<APIGatewayProxyResult> {
-    
-    const lambdaRequestId = context.awsRequestId;
-    const apiRequestId = event.requestContext.requestId;
-    const method = event.httpMethod;
+  event: APIGatewayProxyEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> {
+  const lambdaRequestId = context.awsRequestId;
+  const apiRequestId = event.requestContext.requestId;
+  const method = event.httpMethod;
 
-    console.log(`API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`);
+  console.log(
+    `API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`
+  );
 
-    // Não precisa verificar o resource pois sempre será chamado por "/orders"
-    if(method === "GET") {
-        console.log("GET /orders chamado");
-        const email =  event.queryStringParameters!.email;
-        const orderId =  event.queryStringParameters!.orderId;
+  // Não precisa verificar o resource pois sempre será chamado por "/orders"
+  if (method === "GET") {
+    if (event.queryStringParameters) {
+      console.log("GET /orders chamado");
+      const email = event.queryStringParameters.email;
+      const orderId = event.queryStringParameters.orderId;
 
-        if (email) {
-            if (orderId) {
+      if (email) {
+        // Busca um pedido de um usuário
+        if (orderId) {
+          try {
+            const order = await orderRepository.getOrder(email, orderId);
 
-            } else {
+            return {
+              statusCode: 200,
+              body: JSON.stringify(convertToOrderResponse(order)),
+            };
+          } catch (error) {
+            console.log((<Error>error).message);
+            return {
+              statusCode: 404,
+              body: (<Error>error).message,
+            };
+          }
+        } else {
+          // Busca todos os pedidos de um usuário
+          const orders = await orderRepository.getOrdersByEmail(email);
 
-            }
+          return {
+            statusCode: 200,
+            body: JSON.stringify(orders.map(convertToOrderResponse)),
+          };
         }
+      }
+    } else {
+      const orders = await orderRepository.getAllOrders();
 
-    } else if(method === "POST") {
-        console.log("POST /orders chamado");
-    } else if(method === "DELETE") {
-        console.log("DELETE /orders chamado");
-        const email =  event.queryStringParameters!.email;
-        const orderId =  event.queryStringParameters!.orderId;
+      return {
+        statusCode: 200,
+        body: JSON.stringify(orders.map(convertToOrderResponse)),
+      };
     }
+  } else if (method === "POST") {
+    console.log("POST /orders chamado");
+    const orderRequest = JSON.parse(event.body!) as OrderRequest;
+    const products = await productRepository.getProductByIds(
+      orderRequest.productIds
+    );
 
-    return {
-        statusCode: 400,
-        body: JSON.stringify({
-            message: "Bad request"
-        })
+    if (products.length === orderRequest.productIds.length) {
+      const order = buildOrder(orderRequest, products);
+      const orderCreated = await orderRepository.createOrder(order);
+
+      return {
+        statusCode: 201,
+        body: JSON.stringify(convertToOrderResponse(orderCreated)),
+      };
+    } else {
+      return {
+        statusCode: 404,
+        body: "One or more products were not found",
+      };
     }
+  } else if (method === "DELETE") {
+    console.log("DELETE /orders chamado");
+    const email = event.queryStringParameters!.email!;
+    const orderId = event.queryStringParameters!.orderId!;
+
+    try {
+      const orderDelete = await orderRepository.deleteOrder(email, orderId);
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(convertToOrderResponse(orderDelete)),
+      };
+    } catch (error) {
+      console.log((<Error>error).message);
+      return {
+        statusCode: 404,
+        body: (<Error>error).message,
+      };
+    }
+  }
+
+  return {
+    statusCode: 400,
+    body: JSON.stringify({
+      message: "Bad request",
+    }),
+  };
+}
+
+function convertToOrderResponse(order: Order): OrderResponse {
+  const orderProducts: OrderProductResponse[] = [];
+
+  order.products.forEach((product) => {
+    orderProducts.push({
+      code: product.code,
+      price: product.price,
+    });
+  });
+
+  const orderResponse: OrderResponse = {
+    email: order.pk,
+    id: order.sk!,
+    createdAt: order.createdAt!,
+    products: orderProducts,
+    billing: {
+      payment: order.billing.payment as PaymentType,
+      totalPrice: order.billing.totalPrice,
+    },
+    shipping: {
+      type: order.shipping.type as ShippingType,
+      carrier: order.shipping.carrier as CarrierType,
+    },
+  };
+
+  return orderResponse;
 }
 
 function buildOrder(orderRequest: OrderRequest, products: Product[]): Order {
-    const orderProducts: OrderProductResponse[] = [];
+  const orderProducts: OrderProductResponse[] = [];
 
-    let totalPrice = 0;
+  let totalPrice = 0;
 
-    products.forEach((product) => {
-        totalPrice += product.price;
-        orderProducts.push({
-            code: product.code,
-            price: product.price
-        })
-    })
+  products.forEach((product) => {
+    totalPrice += product.price;
+    orderProducts.push({
+      code: product.code,
+      price: product.price,
+    });
+  });
 
-    const order: Order = {
-        pk: orderRequest.email,
-        billing: {
-            payment: orderRequest.payment,
-            totalPrice: totalPrice
-        },
-        shipping: {
-            type: orderRequest.shipping.type,
-            carrier: orderRequest.shipping.carrier
-        },
-        products: orderProducts
-    }
+  const order: Order = {
+    pk: orderRequest.email,
+    billing: {
+      payment: orderRequest.payment,
+      totalPrice: totalPrice,
+    },
+    shipping: {
+      type: orderRequest.shipping.type,
+      carrier: orderRequest.shipping.carrier,
+    },
+    products: orderProducts,
+  };
 
-    return order;
+  return order;
 }
